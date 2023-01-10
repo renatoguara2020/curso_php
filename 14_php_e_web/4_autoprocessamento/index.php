9bc9fdcc8c3ea940c40c7ab01fcbe47c491d0806
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-10 20:36:14 -0300

    OK

--- 14_php_e_web/4_autoprocessamento/index.php
+++ 14_php_e_web/4_autoprocessamento/index.php
@@ -2,38 +2,44 @@
 
   $method = $_SERVER['REQUEST_METHOD'];
 
-  if(isset($_POST['nome'])) {
+  if(isset($_POST['nome']) && ($_POST['nome'] != null)) {
 
     $nome = $_POST['nome'];
 
+    if(isset($_POST['email']) && ($_POST['email'] != null)) {
+      $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    }
   }
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
+
 <body>
-  <?php
+    <?php
     if($method == 'GET'):
   ?>
-  <form action="index.php" method="POST">
-    <div>
-      <input type="text" name="nome" placeholder="Digite seu nome">
-    </div>
-    <div>
-      <input type="submit" value=Enviar>
-    </div>
-  </form>
-  <?php
+    <form action="index.php" method="POST">
+        <div>
+            <input type="text" name="nome" placeholder="Digite seu nome">
+        </div>
+        <div>
+            <input type="submit" value=Enviar>
+        </div>
+    </form>
+    <?php
     else:
   ?>
-    <h1>O seu nome é <?= $nome ?></h1>    
-  <?php
+    <h1>O seu nome é <?= $nome ?></h1>
+    <?php
     endif;
   ?>
 </body>
+
 </html>
