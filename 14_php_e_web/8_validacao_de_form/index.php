298a095e350386ae58b74df9b977e2cd2c63a861
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-10 20:31:16 -0300

    ok

--- 14_php_e_web/8_validacao_de_form/index.php
+++ 14_php_e_web/8_validacao_de_form/index.php
@@ -21,35 +21,38 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
+
 <body>
-  <?php if(count($validacoes)): ?>
+    <?php if(count($validacoes)): ?>
     <ul>
-      <?php foreach($validacoes as $validacao): ?>
+        <?php foreach($validacoes as $validacao): ?>
         <li><?= $validacao ?></li>
-      <?php endforeach; ?>
+        <?php endforeach; ?>
     </ul>
-  <?php endif; ?>
-  <form action="index.php" method="POST">
-    <div>
-      <input type="text" name="nome" placeholder="Digite o seu nome">
-    </div>
-    <div>
-      <input type="text" name="email" placeholder="Digite o seu e-mail">
-    </div>
-    <div>
-      <input type="password" name="senha" placeholder="Digite a sua senha">
-    </div>
-    <div>
-      <input type="password" name="confirmacao" placeholder="Confirme a sua senha">
-    </div>
-    <div>
-      <input type="submit" value="Enviar">
-    </div>
-  </form>
+    <?php endif; ?>
+    <form action="index.php" method="POST">
+        <div>
+            <input type="text" name="nome" placeholder="Digite o seu nome">
+        </div>
+        <div>
+            <input type="text" name="email" placeholder="Digite o seu e-mail">
+        </div>
+        <div>
+            <input type="password" name="senha" placeholder="Digite a sua senha">
+        </div>
+        <div>
+            <input type="password" name="confirmacao" placeholder="Confirme a sua senha">
+        </div>
+        <div>
+            <input type="submit" value="Cadastrar">
+        </div>
+    </form>
 </body>
+
 </html>
